138ae1e2c9c40ea678e94cea21a401843943ae1d
Enzo Bozzani <bozzanienzo@gmail.com>
2024-07-08 01:12:59 -0300

    refac: refac way to authenticate user in routes

--- Controllers/SheetController.cs
+++ Controllers/SheetController.cs
@@ -26,21 +26,137 @@ namespace FinnanciaCSharp.Controllers
             _financeRepository = financeRepository;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetSheets()
+        {
+            try
+            {
+                var userId = User.GetUserId();
+                var user = await _userManager.FindByIdAsync(userId == null ? "" : userId);
+
+                if (user == null || userId == null)
+                {
+                    return Unauthorized("Não autorizado");
+                }
+
+                var sheets = await _sheetRepository.GetSheetsByUserIdAsync(userId);
+
+                return Ok(new { sheets = sheets, isInitialAmountSet = user.IsInitialAmountSet });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSheet([FromBody] CreateSheetDTO createSheetDTO)
+        {
+            var currentDate = DateTime.Now;
+
+            var year = createSheetDTO.Year;
+            var month = createSheetDTO.Month;
+
+            if (year < currentDate.Year || year > currentDate.Year + 1)
+            {
+                return BadRequest("O ano deve ser entre o ano atual ou o próximo");
+            }
+
+            try
+            {
+                var userId = User.GetUserId();
+                var user = await _userManager.FindByIdAsync(userId == null ? "" : userId);
+
+                if (user == null || userId == null)
+                {
+                    return Unauthorized("Não autorizado");
+                }
+
+                var sheetExists = await _sheetRepository.SheetExistsByMonthAndYearAsync(month, year, userId);
+
+                if (sheetExists)
+                {
+                    return BadRequest("Planilha já existente");
+                }
+
+                var newSheet = createSheetDTO.ToSheetFromCreateSheetDTO(userId);
+
+                await _sheetRepository.CreateAsync(newSheet);
+
+                return CreatedAtAction(nameof(GetSheetById), new { id = newSheet.Id }, newSheet.ToSheetDTOFromSheet());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSheetById([FromRoute] Guid id)
+        {
+            try
+            {
+                var userId = User.GetUserId();
+                var user = await _userManager.FindByIdAsync(userId == null ? "" : userId);
+
+                if (user == null || userId == null)
+                {
+                    return Unauthorized("Não autorizado");
+                }
+
+                var sheet = await _sheetRepository.GetSheetByIdAsync(id);
+
+                if (sheet == null)
+                {
+                    return NotFound("Planilha não encontrada!");
+                }
+
+                return Ok(sheet.ToSheetDTOFromSheet());
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSheet([FromRoute] Guid id)
+        {
+            try
+            {
+                var userId = User.GetUserId();
+                var user = await _userManager.FindByIdAsync(userId == null ? "" : userId);
+
+                if (user == null || userId == null)
+                {
+                    return Unauthorized("Não autorizado");
+                }
+
+                var sheet = await _sheetRepository.DeleteSheetAsync(id);
+
+                if (sheet == null)
+                {
+                    return NotFound("Planilha não encontrada");
+                }
+
+                return Ok($"Planilha {sheet.Name} deletada com sucesso!");
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(500, e.Message);
+            }
+        }
+
         [HttpPost("{id}/finance")]
         public async Task<IActionResult> CreateFinance([FromRoute] Guid id, [FromBody] CreateFinanceDTO createFinanceDTO)
         {
             try
             {
                 var userId = User.GetUserId();
+                var user = await _userManager.FindByIdAsync(userId == null ? "" : userId);
 
-                if (userId == null)
-                {
-                    return Unauthorized("Não autorizado");
-                }
-
-                var user = await _userManager.FindByIdAsync(userId);
-
-                if (user == null)
+                if (user == null || userId == null)
                 {
                     return Unauthorized("Não autorizado");
                 }
@@ -85,150 +201,13 @@ namespace FinnanciaCSharp.Controllers
 
                 await _financeRepository.CreateAsync(finance);
 
+                //TODO: totalAmounts: criar novo metodo no sheetRepository e criar um userRepository
+
                 return Ok(finance.ToFinanceDTO());
             }
             catch (Exception e)
             {
 
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteSheet([FromRoute] Guid id)
-        {
-            try
-            {
-                var userId = User.GetUserId();
-
-                if (userId == null)
-                {
-                    return Unauthorized("Não autorizado");
-                }
-
-                var user = await _userManager.FindByIdAsync(userId);
-
-                if (user == null)
-                {
-                    return Unauthorized("Não autorizado");
-                }
-
-                var sheet = await _sheetRepository.DeleteSheetAsync(id);
-
-                if (sheet == null)
-                {
-                    return NotFound("Planilha não encontrada");
-                }
-
-                return Ok($"Planilha {sheet.Name} deletada com sucesso!");
-            }
-            catch (Exception e)
-            {
-
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetSheets()
-        {
-            try
-            {
-                var userId = User.GetUserId();
-
-                if (userId == null)
-                {
-                    return Unauthorized("Não autorizado");
-                }
-
-                var user = await _userManager.FindByIdAsync(userId);
-
-                if (user == null)
-                {
-                    return Unauthorized("Não autorizado");
-                }
-
-                var sheets = await _sheetRepository.GetSheetsByUserIdAsync(userId);
-
-                return Ok(new { sheets = sheets, isInitialAmountSet = user.IsInitialAmountSet });
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetSheetById([FromRoute] Guid id)
-        {
-            try
-            {
-                var userId = User.GetUserId();
-
-                if (userId == null)
-                {
-                    return Unauthorized("Não autorizado!");
-                }
-
-                var user = await _userManager.FindByIdAsync(userId);
-
-                if (user == null)
-                {
-                    return Unauthorized("Não autorizado!");
-                }
-
-                var sheet = await _sheetRepository.GetSheetByIdAsync(id);
-
-                if (sheet == null)
-                {
-                    return NotFound("Planilha não encontrada!");
-                }
-
-                return Ok(sheet.ToSheetDTOFromSheet());
-            }
-            catch (Exception e)
-            {
-                return StatusCode(500, e.Message);
-            }
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateSheet([FromBody] CreateSheetDTO createSheetDTO)
-        {
-            var currentDate = DateTime.Now;
-
-            var year = createSheetDTO.Year;
-            var month = createSheetDTO.Month;
-
-            if (year < currentDate.Year || year > currentDate.Year + 1)
-            {
-                return BadRequest("O ano deve ser entre o ano atual ou o próximo");
-            }
-
-            try
-            {
-                var userId = User.GetUserId();
-
-                if (userId == null)
-                {
-                    return Unauthorized("Não autorizado");
-                }
-
-                var sheetExists = await _sheetRepository.SheetExistsByMonthAndYearAsync(month, year, userId);
-
-                if (sheetExists)
-                {
-                    return BadRequest("Planilha já existente");
-                }
-
-                var newSheet = createSheetDTO.ToSheetFromCreateSheetDTO(userId);
-
-                await _sheetRepository.CreateAsync(newSheet);
-
-                return CreatedAtAction(nameof(GetSheetById), new { id = newSheet.Id }, newSheet.ToSheetDTOFromSheet());
-            }
-            catch (Exception e)
-            {
                 return StatusCode(500, e.Message);
             }
         }
